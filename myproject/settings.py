93a5eaa1b3e2ab1da98728ed6f4507302a63ec56
Rowland <rowlandobi1980@gmail.com>
2025-09-10 12:06:00 +0100

    settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['myproject.onrender.com']
+ALLOWED_HOSTS = ['https://bookwise-expense-tracker-15.onrender.com']
 
 
 
